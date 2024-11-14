15a2d3b05307bba8f5d9c0a0eeb99439a6d1e9ec
unknown <m.dokhy5@gmail.com>
2024-11-14 20:40:18 +0200

    update file

--- lib/Features/Splash/presentation/views/widgets/splash_view_body.dart
+++ lib/Features/Splash/presentation/views/widgets/splash_view_body.dart
@@ -1,6 +1,9 @@
+import 'package:bookly_app/Features/Home/presentation/views/home_view.dart';
 import 'package:bookly_app/Features/Splash/presentation/views/widgets/sliding_text.dart';
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_navigation/get_navigation.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -15,11 +18,10 @@ class _SplashViewBodyState extends State<SplashViewBody>with SingleTickerProvide
   @override
   void initState() {
     super.initState();
-    animationController =AnimationController(vsync: this,duration: const Duration(seconds: 2));
-    slaidingAnimatin = Tween<Offset>(begin:  const Offset(0.0, 20), end:  Offset.zero).animate(animationController);
-    animationController.forward();
-  
+    initslidinganimation(); 
+    navigateToHome(); 
   }
+
   @override
   void dispose() {
     super.dispose();
@@ -37,5 +39,16 @@ class _SplashViewBodyState extends State<SplashViewBody>with SingleTickerProvide
         Slidingtext(slaidingAnimatin: slaidingAnimatin),
       ],
     );
+  }
+  void initslidinganimation() {
+     animationController =AnimationController(vsync: this,duration: const Duration(seconds: 2));
+    slaidingAnimatin = Tween<Offset>(begin:  const Offset(0.0, 20), end:  Offset.zero).animate(animationController);
+    animationController.forward();
+      
+  }
+  void navigateToHome() {
+    Future.delayed(const Duration(seconds: 3), () {
+      Get.to(()=>const HomeView());
+    });
   }
 }
--- lib/constants.dart
+++ lib/constants.dart
@@ -2,3 +2,4 @@ import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color(0xff100B20);
 const kSecondaryColor = Color(0xffF7F7F7);
+const ktransationDuration = Duration(milliseconds: 250);
