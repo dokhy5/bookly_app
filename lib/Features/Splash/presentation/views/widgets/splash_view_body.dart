2c79a0fb2877370425626f6cb82d274c230d9d67
unknown <m.dokhy5@gmail.com>
2024-11-14 20:15:34 +0200

    update file

--- lib/Features/Splash/presentation/views/widgets/splash_view_body.dart
+++ lib/Features/Splash/presentation/views/widgets/splash_view_body.dart
@@ -1,15 +1,40 @@
+import 'package:bookly_app/Features/Splash/presentation/views/widgets/sliding_text.dart';
+import 'package:bookly_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
 
   @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>with SingleTickerProviderStateMixin{
+  late AnimationController animationController;
+  late Animation<Offset> slaidingAnimatin;
+  @override
+  void initState() {
+    super.initState();
+    animationController =AnimationController(vsync: this,duration: const Duration(seconds: 2));
+    slaidingAnimatin = Tween<Offset>(begin:  const Offset(0.0, 20), end:  Offset.zero).animate(animationController);
+    animationController.forward();
+  
+  }
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
+  @override
   Widget build(BuildContext context) {
-    return const Column(
+    return  Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-      //  Image.asset(AssetsData.logo),
+        
+       Image.asset(AssetsData.logo,),
+       const SizedBox(height: 10,),
+        Slidingtext(slaidingAnimatin: slaidingAnimatin),
       ],
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:bookly_app/Features/Splash/presentation/views/splash_view.dart';
+import 'package:bookly_app/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
@@ -12,9 +13,12 @@ class Bookly extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp(
+    return  GetMaterialApp(
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: kPrimaryColor,
+        ),
       debugShowCheckedModeBanner: false,
-      home: SplashView(),
+      home: const SplashView(),
       
     );
 
