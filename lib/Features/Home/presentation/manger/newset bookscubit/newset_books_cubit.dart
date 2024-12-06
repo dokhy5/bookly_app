5ab7cc23d1536270d93759e3fe06922692b5fc34
dokhy5 <m.dokhy5@gmail.com>
2024-12-06 05:08:53 +0200

    create newset books state and cubit

---
+++ lib/Features/Home/presentation/manger/newset bookscubit/newset_books_cubit.dart
@@ -0,0 +1,20 @@
+import 'package:bloc/bloc.dart';
+import 'package:bookly_app/Features/Home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/Features/Home/data/repos/home_repo.dart';
+import 'package:equatable/equatable.dart';
+
+part 'newset_books_state.dart';
+
+class NewsetBooksCubit extends Cubit<NewsetBooksState> {
+  NewsetBooksCubit(this.homeRepo) : super(NewsetBooksInitial());
+   final HomeRepo homeRepo;
+  Future<void> fetchNewsetBooks() async {
+    emit(NewsetBooksLoading());
+    var result = await homeRepo.fetchNewsetBooks();
+    result.fold(
+      (failure) => emit(NewsetBooksFailure(failure.errmessge)),
+      (books) => emit(NewsetBooksSuccess(books)),
+    );
+  }
+
+}
